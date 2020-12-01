641ae75cb8b0ae67ac70597be4c87e92bf5c061f
Victor <victor.olsson@kollmorgen.com>
2020-12-01 09:32:31 +0100

    solved first puzzles

--- day1.py
+++ day1.py
@@ -1,22 +1,65 @@
+"""
+--- Day 1: Report Repair ---
+After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
+
+The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
+
+To save your vacation, you need to get all fifty stars by December 25th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+
+Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+
+For example, suppose your expense report contained the following:
+
+1721
+979
+366
+299
+675
+1456
+In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+
+Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+
+Your puzzle answer was 494475.
+"""
 with open('day1_input.txt','r') as f:
     data = f.read()
 
 data = [int(i) for i in data.split()]
-def first_problem(data):
-    for i in data:
-        for j in data:
-            if i + j == 2020:
-                print(i*j)
-                break
-  
-def second_problem(data):
-    for i in data:
-        for j in data:
-            for k in data:
-                if i + j + k == 2020:
-                    print(i*j*k)
-                    break
+data = set(data)
 
-if __name__ == '__main__':
-    first_problem(data)
-    second_problem(data)
+def first_problem(data, val):
+    for i in data:
+        if (val - i) in data:
+            return i, (val-i)                    
+
+i,j = first_problem(data, 2020)
+print('First solution is: {}'.format(i*j))
+"""
+--- Part Two ---
+The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to 2020?
+
+Your puzzle answer was 267520550.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
+At this point, you should return to your Advent calendar and try another puzzle.
+
+If you still want to see it, you can get your puzzle input.
+"""
+
+def second_problem(data, val):
+    for i in data:
+        for j in data:
+            if (val - (i + j)) in data:
+                return i, j, val - (i + j)
+i,j,k = second_problem(data, 2020)
+print('The second solution is: {} '.format(i*j*k))
